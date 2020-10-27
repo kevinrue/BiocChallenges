038f620161c5c04eca00086ebdd4461537b95662
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-10-27 17:52:19 +0000

    add function to process and format challenge difficulty

---
+++ R/params.R
@@ -0,0 +1,37 @@
+
+#' @rdname format_difficulty
+#' @export
+#' @format
+#' `difficulty_levels` is a character vector of valid difficulty levels.
+"difficulty_levels"
+
+difficulty_levels <- c("Novice", "Intermediate", "Advanced")
+
+#' Challenge Difficulty
+#'
+#' @param param Challenge parameters as `list`.
+#'
+#' @return
+#' `format_difficulty()` returns a character value indicating the challenge difficulty.
+#' @export
+#'
+#' @format
+#'
+#' @importFrom stringr str_to_title
+#'
+#' @examples
+#' difficulty_levels
+#'
+#' params <- list(difficulty = "novice")
+#' format_difficulty(params)
+format_difficulty <- function(params) {
+    difficulty <- params$difficulty
+    if (is.null(difficulty)) {
+        stop("Challenge difficulty is missing")
+    }
+    difficulty <- str_to_title(difficulty)
+    if (!difficulty %in% difficulty_levels) {
+        stop("Challenge difficult should be one of ", deparse(difficulty_levels))
+    }
+    difficulty
+}
